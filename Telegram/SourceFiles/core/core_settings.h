940a71eba9c7f2a18213cd995ee48851f37a23cc
megapro17 <megapro17@gmail.com>
2024-03-22 15:14:10 +0300

    Enable native window frame by default

--- Telegram/SourceFiles/core/core_settings.h
+++ Telegram/SourceFiles/core/core_settings.h
@@ -957,7 +957,7 @@ private:
 	rpl::variable<float64> _dialogsWidthRatio; // per-window
 	rpl::variable<int> _thirdColumnWidth = kDefaultThirdColumnWidth; // p-w
 	bool _notifyFromAll = true;
-	rpl::variable<bool> _nativeWindowFrame = false;
+	rpl::variable<bool> _nativeWindowFrame = true;
 	rpl::variable<std::optional<bool>> _systemDarkMode = std::nullopt;
 	rpl::variable<bool> _systemDarkModeEnabled = false;
 	rpl::variable<WindowTitleContent> _windowTitleContent;
